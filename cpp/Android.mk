b8631c4467a6658e2d7b2e2f319bb4a08466300a
Bernhard Rosenkraenzer <Bernhard.Rosenkranzer@linaro.org>
2012-11-15 22:54:42 +0100

    rs: Use -Wno-error - gcc 4.7 adds new warnings rs can't deal with
    Change-Id: I38fe66629552d6f11be1bb69d5a93a05251614df
    Signed-off-by: Bernhard Rosenkraenzer <Bernhard.Rosenkranzer@linaro.org>

--- cpp/Android.mk
+++ cpp/Android.mk
@@ -20,7 +20,7 @@ LOCAL_MODULE:= libRScpp
 
 LOCAL_MODULE_TAGS := optional
 
-LOCAL_CFLAGS := -Qunused-arguments -Wno-unknown-warning-option
+LOCAL_CFLAGS := $(call-cc-cpp-option,-Qunused-arguments) -Wno-unknown-warning-option
 
 intermediates := $(call intermediates-dir-for,STATIC_LIBRARIES,libRS,TARGET,)
 librs_generated_headers := \
